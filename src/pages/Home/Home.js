8fc18e0440460cc609eb686983390767854781cc
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-06-28 22:10:04 +0530

    used InfiniteScroll component provided by react  to retrive data in chuncks

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,61 @@
-import { HotelCard, Navbar } from "../../components";
+import { useState, useEffect } from 'react'
+import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
+
+import { HotelCard, Navbar } from "../../components";
 import './Home.css'
 
 
-import React from 'react'
-
 export const Home = () => {
-    const [hotels, setHotels] = React.useState([])
+    const [hasMore, setHasMore] = useState(true)
+    const [currentIndex, setCurrentIndex] = useState(15)
+    const [testData, setTestData] = useState([])
+    const [hotels, setHotels] = useState([])
 
-    React.useEffect(() => {
+    useEffect(() => {
         (
             async () => {
                 const response = await axios.get('http://localhost:8000/api/hotels')
-                setHotels(response.data)
+                setTestData(response.data)
+                setHotels(response.data ? response.data.slice(0, currentIndex) : [])
             }
         )()
-    }, [])
+    }, [hotels, currentIndex])
+
+    const fetchMoreData = () => {
+        if (hotels.length === testData.length) {
+            setHasMore(false)
+            return
+        }
+        setTimeout(() => {
+            if (hotels && hotels.length > 0) {
+                setHotels(hotels.concat(testData.slice(currentIndex, currentIndex + 15)))
+                setCurrentIndex(currentIndex + 15)
+            } else {
+                setHotels([])
+            }
+        }, 1000)
+    }
     return (
         <>
             <Navbar />
-            <main className="main">
-                {hotels && hotels.map(hotel => <HotelCard key={hotel._id} hotel={hotel} />)}
-            </main>
+            {
+                hotels && hotels.length > 0 ? (
+                    <InfiniteScroll
+                        dataLength={hotels.length}
+                        next={fetchMoreData}
+                        hasMore={hasMore}
+                        loader={hotels.length > 0 && <h4 className="alert-text">Loading...</h4>}
+                        endMessage={
+                            <p className='alert-text'>Noting more</p>
+                        }
+                    >
+                        <main className="main">
+                            {hotels && hotels.map(hotel => <HotelCard key={hotel._id} hotel={hotel} />)}
+                        </main>
+
+                    </InfiniteScroll>) : (<></>)
+            }
         </>
 
     )
