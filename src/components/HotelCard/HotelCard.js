d72e7c7dd456420d3ea71f12b6f84b2a54a5232c
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-06-28 21:19:15 +0530

    called backend api to retrive all hotels data and render them inside homepage

--- src/components/HotelCard/HotelCard.js
+++ src/components/HotelCard/HotelCard.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import './HotelCard.css'
 
-export const HotelCard = () => {
+export const HotelCard = ({ hotel }) => {
     const [isSelected, setIsSelected] = React.useState(false)
+
+    const { image, name, price, rating, address, state } = hotel
     return (
         <div className="relative hotelcard-container shadow cursor-pointer">
             <div >
-                <img className="img" src={"https://a0.muscache.com/im/pictures/miso/Hosting-26117817/original/9da40e3c-5846-4359-bb41-05c27b09a8f5.jpeg?im_w=720"} alt={"hotel"} />
+                <img className="img" src={image} alt={"hotel"} />
                 <div className="hotelcard-details">
                     <div className="d-flex align-center">
                         <span className="location">
-                            {"Boring Road"}, {"Bihar"}
+                            {address}, {state}
                         </span>
                         <span className="rating d-flex align-center">
                             <span class="material-symbols-outlined">star</span>
-                            <span>{5}</span>
+                            <span>{rating}</span>
                         </span>
                     </div>
-                    <p className="hotel-name">{"NewHotel"}</p>
+                    <p className="hotel-name">{name}</p>
                     <p className="price-details">
-                        <span className="price">Rs. {500}</span>
+                        <span className="price">Rs. {price}/</span>
                         <span>night</span>
                     </p>
                 </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,26 @@
 import { HotelCard, Navbar } from "../../components";
+import axios from "axios";
 import './Home.css'
 
 
 import React from 'react'
 
 export const Home = () => {
+    const [hotels, setHotels] = React.useState([])
+
+    React.useEffect(() => {
+        (
+            async () => {
+                const response = await axios.get('http://localhost:8000/api/hotels')
+                setHotels(response.data)
+            }
+        )()
+    }, [])
     return (
         <>
             <Navbar />
             <main className="main">
-                <HotelCard />
+                {hotels && hotels.map(hotel => <HotelCard key={hotel._id} hotel={hotel} />)}
             </main>
         </>
 
