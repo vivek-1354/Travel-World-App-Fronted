00f79fc3ec1ad3c68726e51af0e296406fe586ca
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-07-05 16:02:28 +0530

    removed context provider from filter component and used filterState provided by redux

--- src/components/Filters/Filter.js
+++ src/components/Filters/Filter.js
@@ -1,51 +1,70 @@
-import { useNavigate } from 'react-router-dom'
-import { useFilter } from '../../context'
-import './Filter.css'
+import { useNavigate } from "react-router-dom";
+import "./Filter.css";
 
-import { FreeCancel, PriceRange, PropertyType, Rating, RoomsAndBeds } from './index'
+import {
+  FreeCancel,
+  PriceRange,
+  PropertyType,
+  Rating,
+  RoomsAndBeds,
+} from "./index";
+import { useDispatch } from "react-redux";
+import {
+  clearFilterData,
+  handleModalOpen,
+} from "../../Redux/actions/filterActions";
 
 export const Filter = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const { filterState, filterDispatch } = useFilter()
+  const handleCloseClick = () => {
+    dispatch(handleModalOpen());
+    navigate("/");
+  };
 
-    const handleCloseClick = () => {
-        filterDispatch({ type: 'HANDAL_MODAL_OPEN' })
-        navigate('/')
-    }
+  const handleClearFilterClick = () => {
+    dispatch(clearFilterData());
+  };
 
-    const handleClearFilterClick = () => {
-        filterDispatch({
-            type: 'CLEAR_ALL'
-        })
-    }
+  const handleApplyClick = () => {
+    dispatch(handleModalOpen());
+  };
 
-    const handleApplyClick = () => {
-        filterDispatch({ type: 'HANDAL_MODAL_OPEN' })
-        console.log({ ...filterState })
-    }
-
-    return (
-        <div className='filter-modal'>
-            <div className="filter-page shadow">
-                <div className='d-flex align-center justify-space-between'>
-                    <span className='filter-label'>Filter</span>
-                    <button className="button btn-filter-close cursor-pointer d-flex align-center justify-center" onClick={handleCloseClick}>
-                        <span className="material-symbols-outlined">close</span>
-                    </button>
-                </div>
-                <div className="filter-content">
-                    <PriceRange />
-                    <RoomsAndBeds />
-                    <PropertyType />
-                    <Rating />
-                    <FreeCancel />
-                    <div className='filter-container d-flex justify-space-between'>
-                        <button className='button cursor btn-link-primary' onClick={handleClearFilterClick}>Clear All</button>
-                        <button className='button cursor btn-primary btn-apply' onClick={handleApplyClick}>Apply</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="filter-modal">
+      <div className="filter-page shadow">
+        <div className="d-flex align-center justify-space-between">
+          <span className="filter-label">Filter</span>
+          <button
+            className="button btn-filter-close cursor-pointer d-flex align-center justify-center"
+            onClick={handleCloseClick}
+          >
+            <span className="material-symbols-outlined">close</span>
+          </button>
         </div>
-    )
-}
+        <div className="filter-content">
+          <PriceRange />
+          <RoomsAndBeds />
+          <PropertyType />
+          <Rating />
+          <FreeCancel />
+          <div className="filter-container d-flex justify-space-between">
+            <button
+              className="button cursor btn-link-primary"
+              onClick={handleClearFilterClick}
+            >
+              Clear All
+            </button>
+            <button
+              className="button cursor btn-primary btn-apply"
+              onClick={handleApplyClick}
+            >
+              Apply
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
